1ea3938bfad6ba4ea851a32af0afa922be82a76a
sankalp khare <kharesankalp57@gmail.com>
2025-12-19 17:00:04 +0530

    Fix routing, EJS rendering, and URL redirect issues

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path  = require('path');
 const {connectToMongoDB}= require("./connect");
 const urlRoute=require('./routes/url');
 const URL=require('./models/url');
+const staticRoute= require("./routes/staticRouter");
+
 
 const app  = express();
 const PORT =8001;
@@ -10,9 +13,24 @@ connectToMongoDB("mongodb://localhost:27017/short-url")
 .then(()=> console.log('Mongodb connected')
 );
 
-app.use(express.json());
+app.set("view engine","ejs");
+app.set('views',path.resolve("./views"));
+
+app.use(express. json());
+app.use(express.urlencoded({extended:false}));
 
 app.use("/url",urlRoute);
+
+app.use("/",staticRoute);
+
+app.get('/test', async (req , res )=>{
+    const allUrls= await URL.find({});
+    return res.render("home",{
+        urls:allUrls,
+    });
+});
+
+
 
 app.get('/:shortId', async (req,res) => {
       const shortId = req.params.shortId;
